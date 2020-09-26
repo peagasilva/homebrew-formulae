3ca71b2b7cde0fc37aae2397f98db5a88edd30f1
Raphael Silva <sr.silvaraphael@gmail.com>
2020-09-26 16:57:43 +0200

    add formula and README file

---
+++ Formula/latest-swift-format.rb
@@ -0,0 +1,17 @@
+class LatestSwiftFormat < Formula
+    desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
+    homepage "https://github.com/apple/swift-format"
+    url "https://github.com/apple/swift-format.git", :branch => "swift-5.3-branch"
+    version "0.50300.0"
+
+    depends_on :xcode => ["12.0", :build]
+
+    def install
+        system "swift build -c release --disable-sandbox --build-path '.build'"
+        bin.install ".build/release/swift-format"
+    end
+
+    test do
+        system "#{bin}/swift-format --version"
+    end
+end
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# homebrew-formulae
+# Homebrew Formulae
+
+A [Homebrew tap] for various projects of mine.
+
+## Usage
+
+Tap this repository:
+
+    brew tap peagasilva/formulae
+
+Install the packages it contains like any other Homebrew package:
+
+    brew install latest-swift-format
+
+[Homebrew tap]: https://docs.brew.sh/Taps
